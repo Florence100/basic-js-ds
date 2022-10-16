f63cf707f9152fbd6bfe5f559be585e3a6a78b3d
anastasia <anastasia.gribovskaia@gmail.com>
2022-10-17 00:44:07 +0300

    stask

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor () {
+    this.arr = [];
+    this.top = null;
+    this.length = 0;
+  }
+
+  push(element) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    this.arr.push(element);
+    this.top = element;
+    this.length++;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    
+    this.length--;
+    this.top = this.arr[this.arr.length - 2];
+    return this.arr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    return this.top;
   }
 }
 
 module.exports = {
   Stack
 };
+
+const stack = new Stack();
+stack.push(5);
+stack.push(6);
+stack.push(7);
+console.log(stack.peek()); // 7
+console.log(stack.pop()); //7
+console.log(stack.peek()); //6
